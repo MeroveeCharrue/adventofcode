a214f7113615a2a0eadcb23b630d02eb26e521d7
Mérovée Charrue <merovee@charrue.org>
2019-12-06 23:03:41 +0100

    Let's count down christmas \o/

---
+++ adventofcode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  adventofcode
+//
+//  Created by Merovee Charrue on 06/12/2019.
+//  Copyright © 2019 Merovee Charrue. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
